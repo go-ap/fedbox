527d0b61751205c30f913421931f67a086aa236a
Marius Orcsik <marius@habarnam.ro>
2019-05-05 00:54:37 +0200

    Save actor and object when saving an activity

--- app/c2s.go
+++ app/c2s.go
@@ -24,9 +24,24 @@ func HandleClientRequest(w http.ResponseWriter, r *http.Request) (as.IRI, int, e
 			return as.IRI(""), http.StatusInternalServerError, errors.NewNotValid(err, "unable to unmarshal JSON request")
 		}
 	}
+	var ob, act as.Item
 	if repo, ok := ContextActivitySaver(r.Context()); ok == true {
 		if it, err = repo.SaveActivity(it); err != nil {
-			return as.IRI(""), http.StatusInternalServerError, errors.Annotatef(err, "Can't save server %s %s", f.Collection, it.GetType())
+			return as.IRI(""), http.StatusInternalServerError, errors.Annotatef(err, "Can't save %s %s", f.Collection, it.GetType())
+		}
+		if a := as.ToActivity(it); ok {
+			ob = a.Object
+			act = a.Actor
+		}
+	}
+	if repo, ok := ContextActorSaver(r.Context()); ok == true && act != nil {
+		if it, err = repo.SaveActor(act); err != nil {
+			return as.IRI(""), http.StatusInternalServerError, errors.Annotatef(err, "Can't save %s %s", f.Collection, act.GetType())
+		}
+	}
+	if repo, ok := ContextObjectSaver(r.Context()); ok == true && ob != nil {
+		if it, err = repo.SaveObject(ob); err != nil {
+			return as.IRI(""), http.StatusInternalServerError, errors.Annotatef(err, "Can't save %s %s", f.Collection, ob.GetType())
 		}
 	}
 
