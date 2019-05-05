bc96b5ad9e2ffc3878d37a4682b2a871db312d9b
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-05-05 21:29:26 +0200

    Using single file for activity processing handlers

--- app/activities.go
+++ app/activities.go
@@ -10,6 +10,31 @@ import (
 	"net/http"
 )
 
+// HandleClientRequest handles client to server (C2S) POST requests to an ActivityPub Actor's outbox
+func HandleClientRequest(w http.ResponseWriter, r *http.Request) (as.IRI, int, error) {
+	f := &st.Filters{}
+	if err := f.FromRequest(r); err != nil {
+		return as.IRI(""), http.StatusBadRequest, errors.BadRequestf("could not load filters from request")
+	}
+
+	var it as.Item
+	var err error
+	if body, err := ioutil.ReadAll(r.Body); err != nil || len(body) == 0 {
+		return as.IRI(""), http.StatusInternalServerError, errors.NewNotValid(err, "unable to read request body")
+	} else {
+		if it, err = as.UnmarshalJSON(body); err != nil {
+			return as.IRI(""), http.StatusInternalServerError, errors.NewNotValid(err, "unable to unmarshal JSON request")
+		}
+	}
+	if repo, ok := context.ActivitySaver(r.Context()); ok == true {
+		if it, err = repo.SaveActivity(it); err != nil {
+			return as.IRI(""), http.StatusInternalServerError, errors.Annotatef(err, "Can't save %s %s", f.Collection, it.GetType())
+		}
+	}
+
+	return it.GetLink(), http.StatusOK, nil
+}
+
 // HandleServerRequest handles server to server (S2S) POST requests to an ActivityPub Actor's inbox
 func HandleServerRequest(w http.ResponseWriter, r *http.Request) (as.IRI, int, error) {
 	f := &st.Filters{}
