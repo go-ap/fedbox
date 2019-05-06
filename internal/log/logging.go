4add3eceeefc4ee6e77c103840e558421ec6b3a8
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-05-06 22:29:39 +0200

    Don't log request start

--- internal/log/logging.go
+++ internal/log/logging.go
@@ -46,25 +46,23 @@ func (l *StructuredLogger) NewLogEntry(r *http.Request) middleware.LogEntry {
 	logFields["ts"] = time.Now().UTC().Format(time.RFC1123)
 
 	if reqID := middleware.GetReqID(r.Context()); reqID != "" {
-		logFields["req_id"] = reqID
+		logFields["id"] = reqID
 	}
 
 	scheme := "http"
 	if r.TLS != nil {
 		scheme = "https"
 	}
-	logFields["http_scheme"] = scheme
-	logFields["http_proto"] = r.Proto
-	logFields["http_method"] = r.Method
+	logFields["scheme"] = scheme
+	logFields["proto"] = r.Proto
+	logFields["method"] = r.Method
 
-	logFields["remote_addr"] = r.RemoteAddr
-	logFields["user_agent"] = r.UserAgent()
+	logFields["addr"] = r.RemoteAddr
+	logFields["ua"] = r.UserAgent()
 
 	logFields["uri"] = fmt.Sprintf("%s://%s%s", scheme, r.Host, r.RequestURI)
 
 	entry.Logger = entry.Logger.WithFields(logFields)
-
-	entry.Logger.Infoln("request started")
 
 	return entry
 }
@@ -75,11 +73,18 @@ type StructuredLoggerEntry struct {
 
 func (l *StructuredLoggerEntry) Write(status, bytes int, elapsed time.Duration) {
 	l.Logger = l.Logger.WithFields(logrus.Fields{
-		"resp_status": status, "resp_bytes_length": bytes,
-		"resp_elapsed_ms": float64(elapsed.Nanoseconds()) / 1000000.0,
+		"status": status,
+		"len": bytes,
+		"elapsed": float64(elapsed.Nanoseconds()) / 1000000.0,
 	})
 
-	l.Logger.Infoln("request complete")
+	var logFn func(args ...interface{})
+	if status > 200 && status < 400 {
+		logFn = l.Logger.Info
+	} else {
+		logFn = l.Logger.Error
+	}
+	logFn(http.StatusText(status))
 }
 
 func (l *StructuredLoggerEntry) Panic(v interface{}, stack []byte) {
