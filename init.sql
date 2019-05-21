fa574dd875072cfb02555e938fb776743eddc7f7
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-05-21 21:06:50 +0200

    Add default for count
    Add default collections in the database

--- init.sql
+++ init.sql
@@ -96,16 +96,20 @@ create table actors
 -- name: create-activitypub-collections
 create table collections (
      "id"  serial not null constraint collections_pkey primary key,
-     "key" varchar constraint collections_key_key unique,
      "iri" varchar constraint collections_iri_key unique,
      "type" types,
      "created_at" timestamptz default current_timestamp,
      "updated_at" timestamptz default NULL,
-     "count" int,
+     "count" int DEFAULT 0,
      "elements" varchar[] default NULL
 );
-
 -- name: insert-service-actor
 insert into actors ("key", "iri", "type", "raw")
 values ('d3ab037c-0f15-4c09-b635-3d6e201c11aa', 'https://federated.id/actors/d3ab037c-0f15-4c09-b635-3d6e201c11aa', 'Service', '{"@context": ["https://www.w3.org/ns/activitystreams"],"id": "https://federated.id/actors/d3ab037c-0f15-4c09-b635-3d6e201c11aa","type": "Service","name": "self","inbox": "https://federated.id/inbox", "audience": [
   "https://www.w3.org/ns/activitystreams#Public"]}');
+-- name: insert-activities-collection
+insert into collections ("iri", "type") values ('https://fedbox.git:4000/activities', 'OrderedCollection');
+-- name: insert-actors-collection
+insert into collections ("iri", "type") values ('https://fedbox.git:4000/actors', 'OrderedCollection');
+-- name: insert-objects-collection
+insert into collections ("iri", "type") values ('https://fedbox.git:4000/objects', 'OrderedCollection');
