cb818225df0bd9759000ab8ccb6c7ac95a63a0cd
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-05-21 11:19:16 +0200

    Added a collections table

--- init.sql
+++ init.sql
@@ -88,10 +88,23 @@ create table actors
     "id"  serial not null constraint actors_pkey primary key,
     "key" varchar constraint actors_key_key unique,
     "iri" varchar constraint actors_iri_key unique,
-    "created_at" timetz default current_timestamp,
+    "created_at" timestamptz default current_timestamp,
+    "updated_at" timestamptz default NULL,
     "type" types,
     "raw" jsonb
 );
+-- name: create-activitypub-collections
+create table collections (
+     "id"  serial not null constraint collections_pkey primary key,
+     "key" varchar constraint collections_key_key unique,
+     "iri" varchar constraint collections_iri_key unique,
+     "type" types,
+     "created_at" timestamptz default current_timestamp,
+     "updated_at" timestamptz default NULL,
+     "count" int,
+     "elements" varchar[] default NULL
+);
+
 -- name: insert-service-actor
 insert into actors ("key", "iri", "type", "raw")
 values ('d3ab037c-0f15-4c09-b635-3d6e201c11aa', 'https://federated.id/actors/d3ab037c-0f15-4c09-b635-3d6e201c11aa', 'Service', '{"@context": ["https://www.w3.org/ns/activitystreams"],"id": "https://federated.id/actors/d3ab037c-0f15-4c09-b635-3d6e201c11aa","type": "Service","name": "self","inbox": "https://federated.id/inbox", "audience": [
