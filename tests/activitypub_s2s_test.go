cbc7b0c691eec726d0c08fe6d7d8eb86b9f95661
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-06-15 12:09:10 +0200

    Minimizing the references to hardcoded test host

--- tests/activitypub_s2s_test.go
+++ tests/activitypub_s2s_test.go
@@ -1,13 +1,14 @@
 package tests
 
 import (
+	"fmt"
 	"testing"
 )
 
 var selfAccount = testAccount{
-	id:         "http://127.0.0.1:9998/actors/d3ab037c-0f15-4c09-b635-3d6e201c11aa",
-	Hash:       "d3ab037c-0f15-4c09-b635-3d6e201c11aa",
-	Handle:     "self",
+	id:     fmt.Sprintf("http://%s/actors/d3ab037c-0f15-4c09-b635-3d6e201c11aa", host),
+	Hash:   "d3ab037c-0f15-4c09-b635-3d6e201c11aa",
+	Handle: "self",
 }
 
 var S2STests = testPairs{}
