24e33d18e7db09f3db3d4e399c24b207ac1dfeb9
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-06-17 18:14:52 +0200

    Remove root bucket for Clean

--- storage/boltdb/bootstrap.go
+++ storage/boltdb/bootstrap.go
@@ -58,3 +58,17 @@ func Bootstrap(path string, rootBucket []byte, baseURL string) error {
 
 	return nil
 }
+
+func Clean(path string, rootBucket []byte ) error {
+	var err error
+	db, err := bolt.Open(path, 0600, nil)
+	if err != nil {
+		return errors.Annotatef(err, "could not open db")
+	}
+	defer db.Close()
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		return tx.DeleteBucket(rootBucket)
+	})
+	return err
+}
