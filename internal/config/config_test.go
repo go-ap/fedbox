1d0a8c588b1e8049f936b28ddeb393ef8e726596
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-06-19 20:06:12 +0200

    Placeholder tests for internal packages

---
+++ internal/config/config_test.go
@@ -0,0 +1,7 @@
+package config
+
+import "testing"
+
+func TestLoadFromEnv(t *testing.T) {
+	t.Skipf("TODO")
+}
---
+++ internal/env/env_test.go
@@ -0,0 +1,23 @@
+package env
+
+import "testing"
+
+func TestType_IsProd(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestType_IsQA(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestType_IsTest(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestValidTypeOrDev(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestValidType(t *testing.T) {
+	t.Skipf("TODO")
+}
