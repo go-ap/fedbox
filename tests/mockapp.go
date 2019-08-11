8d91d455f347ac2959be61044cb191ba23832c7a
Marius Orcsik <marius@habarnam.ro>
2019-08-11 14:24:48 +0200

    Streamlined instantiation of boltdbs

--- tests/mockapp.go
+++ tests/mockapp.go
@@ -7,10 +7,13 @@ import (
 	"github.com/go-ap/fedbox/app"
 	"github.com/go-ap/fedbox/cmd"
 	"github.com/go-ap/fedbox/internal/config"
+	"github.com/go-ap/fedbox/internal/env"
 	"github.com/go-ap/fedbox/internal/log"
 	"github.com/go-ap/fedbox/storage/boltdb"
 	"github.com/go-ap/fedbox/validation"
+	"github.com/go-ap/storage"
 	"github.com/go-chi/chi"
+	"github.com/openshift/osin"
 	"github.com/sirupsen/logrus"
 	"os"
 	"testing"
@@ -27,12 +30,51 @@ func resetDB(t *testing.T, testData bool) string {
 	if err != nil {
 		curPath = os.TempDir()
 	}
-	path := config.GetBoltDBPath(curPath, fmt.Sprintf("%s-%d", host, os.Getpid()), "test")
+	path := config.GetBoltDBPath(curPath, host, "test")
 
 	boltdb.Clean(path, []byte(host))
 	boltdb.Bootstrap(path, []byte(host), apiURL)
+	b, s := getBoldDBs(curPath, "test", logrus.New())
+	o := cmd.OAuth{
+		AuthDB:  s,
+		ActorDB: b,
+	}
+	pw := "hahah"
+	id, _ := o.AddClient(pw, []string{authCallbackURL}, nil)
+
+	tok, err := o.GenAuthToken(id, defaultTestAccount.Handle, nil)
+	if err == nil {
+		defaultTestAccount.authToken = tok
+	}
 
 	return path
+}
+
+func getBoldDBs(dir string, env env.Type, l logrus.FieldLogger) (storage.Loader, osin.Storage) {
+	path := config.GetBoltDBPath(dir, host, "test")
+	b, _ := boltdb.New(boltdb.Config{
+		Path:       path,
+		BucketName: host,
+		LogFn:      func(f logrus.Fields, s string, p ...interface{}) { l.Errorf(s, p...) },
+		ErrFn:      func(f logrus.Fields, s string, p ...interface{}) { l.Infof(s, p...) },
+	}, apiURL)
+
+	pathOauth := config.GetBoltDBPath(dir, fmt.Sprintf("%s-oauth", host), "test")
+	if _, err := os.Stat(pathOauth); os.IsNotExist(err) {
+		err := auth.BootstrapBoltDB(pathOauth, []byte(host))
+		if err != nil {
+			l.Errorf("Unable to create missing boltdb file %s: %s", pathOauth, err)
+		}
+	}
+
+	s := auth.NewBoltDBStore(auth.Config{
+		Path:       pathOauth,
+		BucketName: host,
+		LogFn:      func(f logrus.Fields, s string, p ...interface{}) { l.WithFields(f).Infof(s, p...) },
+		ErrFn:      func(f logrus.Fields, s string, p ...interface{}) { l.WithFields(f).Errorf(s, p...) },
+	})
+
+	return b, s
 }
 
 func runAPP(e string) int {
@@ -43,44 +85,13 @@ func runAPP(e string) int {
 	if err != nil {
 		curPath = os.TempDir()
 	}
-	path := config.GetBoltDBPath(curPath, fmt.Sprintf("%s-%d", host, os.Getpid()), "test")
-	b, err := boltdb.New(boltdb.Config{
-		Path:       path,
-		BucketName: host,
-		LogFn:      func(f logrus.Fields, s string, p ...interface{}) { l.Errorf(s, p...) },
-		ErrFn:      func(f logrus.Fields, s string, p ...interface{}) { l.Infof(s, p...) },
-	}, apiURL)
-	if err != nil {
-		return 1
-	}
-	defer b.Close()
+
+	b, s := getBoldDBs(curPath, "test", l)
 
 	a := app.New(l, "HEAD", e)
 	r := chi.NewRouter()
-	pathOauth := config.GetBoltDBPath(curPath, fmt.Sprintf("%s-oauth-%d", host, os.Getpid()), "test")
-	if _, err := os.Stat(pathOauth); os.IsNotExist(err) {
-		err := auth.BootstrapBoltDB(pathOauth, []byte(host))
-		if err != nil {
-			l.Errorf("Unable to create missing boltdb file %s: %s", pathOauth, err)
-		}
-	}
 
-	oStor := auth.NewBoltDBStore(auth.Config{
-		Path:       pathOauth,
-		BucketName: host,
-		LogFn:      func(f logrus.Fields, s string, p ...interface{}) { l.WithFields(f).Infof(s, p...) },
-		ErrFn:      func(f logrus.Fields, s string, p ...interface{}) { l.WithFields(f).Errorf(s, p...) },
-	})
-	defer oStor.Close()
-	o := cmd.OAuth{
-		AuthDB:  oStor,
-		ActorDB: b,
-	}
-	pw := "hahah"
-	id, _ := o.AddClient(pw, []string{authCallbackURL}, nil)
-
-	defaultTestAccount.authToken, _ = o.GenAuthToken(id, defaultTestAccount.Handle, nil)
-	_oauthServer, _ = osinServer(oStor, l)
+	_oauthServer, _ = osinServer(s, l)
 
 	r.Use(app.Repo(b))
 	r.Use(log.NewStructuredLogger(l))
