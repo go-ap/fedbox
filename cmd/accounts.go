d353674e5909720f3091f0217669b0a285d0baf0
Marius Orcsik <marius@habarnam.ro>
2020-06-01 21:05:59 +0200

    Added a basic export metadata command

--- cmd/accounts.go
+++ cmd/accounts.go
@@ -1,10 +1,18 @@
 package cmd
 
-import "gopkg.in/urfave/cli.v2"
+import (
+	"encoding/json"
+	"fmt"
+	pub "github.com/go-ap/activitypub"
+	"github.com/go-ap/errors"
+	ap "github.com/go-ap/fedbox/activitypub"
+	"github.com/go-ap/fedbox/storage"
+	"gopkg.in/urfave/cli.v2"
+)
 
 var AccountsCmd = &cli.Command{
-	Name:    "accounts",
-	Usage:   "Accounts helper",
+	Name:  "accounts",
+	Usage: "Accounts helper",
 	Subcommands: []*cli.Command{
 		exportAccountsMetadataCmd,
 		importAccountsMetadataCmd,
@@ -19,6 +27,49 @@ var exportAccountsMetadataCmd = &cli.Command{
 
 func exportAccountsMetadata(ctl *Control) cli.ActionFunc {
 	return func(c *cli.Context) error {
+		baseIRI := ap.ActorsType.IRI(pub.IRI(ctl.BaseURL))
+		f := ap.FiltersNew(
+			ap.IRI(baseIRI),
+			ap.Type(pub.PersonType),
+		)
+		col, err := ctl.Storage.LoadCollection(f)
+		if err != nil {
+			return err
+		}
+
+		items := make(pub.ItemCollection, 0)
+		err = pub.OnCollectionIntf(col, func(c pub.CollectionInterface) error {
+			for _, tt := range c.Collection() {
+				items = append(items, tt)
+			}
+			return nil
+		})
+
+		metaLoader, ok := ctl.Storage.(storage.MetadataTyper)
+		if !ok {
+			return errors.Newf("")
+		}
+
+		allMeta := make(map[pub.IRI]storage.Metadata, len(items))
+		for _, it := range items {
+			if it.GetType() == pub.PersonType {
+				m, err := metaLoader.LoadMetadata(it.GetLink())
+				if err != nil {
+					Errf("Error loading metadata for %s: %s", it.GetLink(), err)
+					continue
+				}
+				if m == nil {
+					Errf("Error loading metadata for %s, nil metadata", it.GetLink())
+					continue
+				}
+				allMeta[it.GetLink()] = *m
+			}
+		}
+		bytes, err := json.Marshal(allMeta)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s\n", bytes)
 		return nil
 	}
 }
