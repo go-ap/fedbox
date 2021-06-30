cb6ebafcb757137cff96d3b688f7071b3004046b
Marius Orcsik <marius@federated.id>
2021-06-30 15:25:44 +0200

    Added a basic server to server test

--- tests/s2s_test.go
+++ tests/s2s_test.go
@@ -3,10 +3,61 @@
 package tests
 
 import (
+	"fmt"
+	"net/http"
+	"path"
 	"testing"
+
+	pub "github.com/go-ap/activitypub"
 )
 
-var S2STests = testPairs{}
+var activityCount = 0
+
+func NewActivity(actor *testAccount) actMock {
+	activityCount++
+	id := baseURL + "/" + path.Join("activities", fmt.Sprintf("%d", activityCount))
+	return actMock{
+		Id:       id,
+		ActorId:  actor.Id,
+	}
+}
+
+var S2STests = testPairs{
+	{
+		name:  "CreateActor",
+		mocks: nil,
+		tests: []testPair{
+			{
+				mocks: []string{
+					"mocks/service.json",
+					"mocks/actor-johndoe.json",
+					"mocks/application.json",
+				},
+				req: testReq{
+					met:     http.MethodPost,
+					account: defaultC2SAccount(),
+					urlFn:   InboxURL(defaultC2SAccount()),
+					bodyFn:  loadMockJson("mocks/s2s/create-actor.json", NewActivity(defaultC2SAccount())),
+				},
+				res: testRes{
+					code: http.StatusCreated,
+					val: &objectVal{
+						typ: string(pub.CreateType),
+						act: &objectVal{
+							typ:               string(pub.PersonType),
+							preferredUsername: "johndoe",
+						},
+						obj: &objectVal{
+							typ:               string(pub.PersonType),
+							preferredUsername: "johndoe",
+							name:              "Johnathan Doe",
+						},
+					},
+				},
+			},
+		},
+	},
+}
 
 func Test_S2SRequests(t *testing.T) {
 	runTestSuite(t, S2STests)
