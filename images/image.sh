14864a6bd5ba0d97edc7674cdb2dd7efd0a6da29
Marius Orcsik <marius@habarnam.ro>
2022-08-27 16:01:43 +0200

    Adding shellcheck quotes for bash variables in image creation scripts

--- images/image.sh
+++ images/image.sh
@@ -8,11 +8,11 @@ _listen_port=${PORT:-4000}
 _storage=${STORAGE:-all}
 _version=${VERSION}
 
-_image_name=${1:-fedbox:${_environment}-${_storage}}
+_image_name=${1:-fedbox:"${_environment}-${_storage}"}
 _build_name=${2:-localhost/fedbox/builder}
 
 #FROM fedbox/builder AS builder
-_builder=$(buildah from ${_build_name}:latest)
+_builder=$(buildah from "${_build_name}":latest)
 
 if [[ -z ${_builder} ]]; then
     echo "Unable to find builder image: ${_build_name}"
@@ -33,8 +33,8 @@ echo "Building image ${_image_name} for host=${_hostname} env:${_environment} st
 
 #RUN make ENV=${ENV} STORAGE=${STORAGE} VERSION=${VERSION} all && \
 #    docker/gen-certs.sh ${HOSTNAME}
-buildah run ${_builder} make ENV=${ENV:-dev} STORAGE=${STORAGE:-all} VERSION=${_version} all
-buildah run ${_builder} make -C images fedbox.pem
+buildah run "${_builder}" make ENV="${ENV:-dev}" STORAGE="${STORAGE:-all}" VERSION="${_version}" all
+buildah run "${_builder}" make -C images fedbox.pem
 
 #FROM gcr.io/distroless/static
 _image=$(buildah from gcr.io/distroless/static:latest)
@@ -45,39 +45,39 @@ _image=$(buildah from gcr.io/distroless/static:latest)
 #ARG STORAGE=all
 
 #ENV ENV=${ENV:-dev}
-buildah config --env ENV=${_environment} ${_image}
+buildah config --env ENV="${_environment}" "${_image}"
 #ENV STORAGE_PATH=/storage
-#buildah config --env STORAGE_PATH=/storage ${_image}
-#ENV HOSTNAME=${HOSTNAME:-fedbox}
-buildah config --env HOSTNAME=${_hostname} ${_image}
+#buildah config --env STORAGE_PATH=/storage "${_image}
+#ENV HOSTNAME="${HOSTNAME:-fedbox}"
+buildah config --env HOSTNAME="${_hostname}" "${_image}"
 #ENV LISTEN=:${PORT}
-buildah config --env LISTEN=:${_listen_port} ${_image}
+buildah config --env LISTEN=:"${_listen_port}" "${_image}"
 #ENV KEY_PATH=/etc/ssl/certs/${HOSTNAME}.key
-buildah config --env KEY_PATH=/etc/ssl/certs/fedbox.key ${_image}
-#ENV CERT_PATH=/etc/ssl/certs/${HOSTNAME}.crt
-buildah config --env CERT_PATH=/etc/ssl/certs/fedbox.crt ${_image}
+buildah config --env KEY_PATH=/etc/ssl/certs/fedbox.key "${_image}"
+#ENV CERT_PATH=/etc/ssl/certs/"${HOSTNAME}.crt
+buildah config --env CERT_PATH=/etc/ssl/certs/fedbox.crt "${_image}"
 #ENV HTTPS=true
-buildah config --env HTTPS=true ${_image}
+buildah config --env HTTPS=true "${_image}"
 #ENV STORAGE=${STORAGE:-all}
-buildah config --env STORAGE=${_storage} ${_image}
+buildah config --env STORAGE="${_storage}" "${_image}"
 
 #EXPOSE $PORT
-buildah config --port ${_listen_port} ${_image}
+buildah config --port "${_listen_port}" "${_image}"
 
 #VOLUME /storage
-buildah config --volume /storage ${_image}
+buildah config --volume /storage "${_image}"
 #VOLUME /.env
-buildah config --volume /.env ${_image}
+buildah config --volume /.env "${_image}"
 
 #COPY --from=builder /go/src/app/bin/* /bin/
-buildah copy --from ${_builder} ${_image} /go/src/app/bin/* /bin/
+buildah copy --from "${_builder}" "${_image}" /go/src/app/bin/* /bin/
 #COPY --from=builder /go/src/app/*.key /go/src/app/*.crt /go/src/app/*.pem /etc/ssl/certs/
-buildah copy --from ${_builder} ${_image} /go/src/app/images/fedbox.key /etc/ssl/certs/
-buildah copy --from ${_builder} ${_image} /go/src/app/images/fedbox.crt /etc/ssl/certs/
-buildah copy --from ${_builder} ${_image} /go/src/app/images/fedbox.pem /etc/ssl/certs/
+buildah copy --from "${_builder}" "${_image}" /go/src/app/images/fedbox.key /etc/ssl/certs/
+buildah copy --from "${_builder}" "${_image}" /go/src/app/images/fedbox.crt /etc/ssl/certs/
+buildah copy --from "${_builder}" "${_image}" /go/src/app/images/fedbox.pem /etc/ssl/certs/
 
 #CMD ["/bin/fedbox"]
-buildah config --entrypoint '["/bin/fedbox"]' ${_image}
+buildah config --entrypoint '["/bin/fedbox"]' "${_image}"
 
 # commit
-buildah commit ${_image} ${_image_name}
+buildah commit "${_image}" "${_image_name}"
