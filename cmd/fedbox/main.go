33ff72dbe20dc9d61a1de76d15e8b986d7a22d01
Marius Orcsik <marius@federated.id>
2022-11-17 10:17:12 +0800

    Don't overwrite version if go version is (devel)

--- cmd/fedbox/main.go
+++ cmd/fedbox/main.go
@@ -10,7 +10,7 @@ import (
 var version = "HEAD"
 
 func main() {
-	if build, ok := debug.ReadBuildInfo(); ok && version == "HEAD" {
+	if build, ok := debug.ReadBuildInfo(); ok && version == "HEAD" && build.Main.Version != "(devel)" {
 		version = build.Main.Version
 	}
 	if err := cmd.NewApp(version).Run(os.Args); err != nil {
