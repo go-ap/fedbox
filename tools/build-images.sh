25a64b0c6a2dd751d3fc6085e31690037727e8be
Marius Orcsik <marius@federated.id>
2022-12-01 09:19:47 +0800

    Making the build-image script better

--- tools/build-images.sh
+++ tools/build-images.sh
@@ -1,23 +1,16 @@
 make -C images builder
+_storage=${1:-all}
+_push=${2:-build}
 
-_push() {
-    _storage=${1:-all}
-    _branch=${GITHUB_REF#"refs/heads/"}
-    make -C images STORAGE="${_storage}" VERSION="${_branch}" push
-    if [ "${_branch}" == "master" ]; then
-        _branch=$(printf "%s-%s" "${_branch}" "$(git rev-parse --short HEAD)")
-        make -C images STORAGE="${_storage}" ENV=qa VERSION="${_branch}" push
-    fi
+_branch=$(git branch --show-current)
+make -C images STORAGE="${_storage}" VERSION="${_branch}" push
+if [ "${_branch}" == "master" ]; then
+    _branch=$(printf "%s-%s" "${_branch}" "$(git rev-parse --short HEAD)")
+    make -C images STORAGE="${_storage}" ENV=qa VERSION="${_branch}" ${_push}
+fi
 
-    _tag=$(git describe --long --tags || true)
-    if [ -n "${_tag}" ]; then
-        make -C images STORAGE="${_storage}" ENV=prod VERSION="${_tag}" push
-    fi
-}
-
-_push
-_push fs
-_push badger
-_push boltdb
-_push sqlite
+_tag=$(git describe --long --tags || true)
+if [ -n "${_tag}" ]; then
+    make -C images STORAGE="${_storage}" ENV=prod VERSION="${_tag}" ${_push}
+fi
 
