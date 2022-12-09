5d384f0d67bbe34ee20289ba355eb7b18d60739d
Marius Orcsik <marius@federated.id>
2022-12-09 20:45:55 +0800

    Adding lw dep to check

--- tools/update.sh
+++ tools/update.sh
@@ -7,12 +7,14 @@ for dep in ${deps[@]}; do
     sha=$(git --git-dir="../go-ap/${dep}/.git" log -n1 --format=tformat:%h)
     go get -u github.com/go-ap/${dep}@${sha}
 done
-if [[ -d "../wrapper" ]]; then
-    sha=$(git --git-dir="../wrapper/.git" log -n1 --format=tformat:%h)
-    go get -u git.sr.ht/~mariusor/wrapper@${sha}
-fi
-deps=(render)
 
+deps=(wrapper lw)
+for dep in ${deps[@]}; do
+    sha=$(git --git-dir="../${dep}/.git" log -n1 --format=tformat:%h)
+    go get -u git.sr.ht/~mariusor/${dep}@${sha}
+done
+
+deps=(render)
 for dep in ${deps[@]}; do
     sha=$(git --git-dir="../${dep}/.git" log -n1 --format=tformat:%h)
     go get -u github.com/mariusor/${dep}@${sha}
