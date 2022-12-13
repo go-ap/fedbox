b82c28c17bd5d3a27414a1c89565a28706e36249
Marius Orcsik <marius@federated.id>
2022-12-13 20:46:09 +0800

    Streamline the image build and adjacent scripts

--- tools/build-images.sh
+++ tools/build-images.sh
@@ -1,11 +1,16 @@
+#!/bin/sh
+
+set -ex
+
 make -C images builder
 _storage=${1:-all}
 _push=${2:-build}
 
+_sha=$(git rev-parse --short HEAD)
 _branch=$(git branch --show-current)
-make -C images STORAGE="${_storage}" VERSION="${_branch}" push
-if [ "${_branch}" == "master" ]; then
-    _branch=$(printf "%s-%s" "${_branch}" "$(git rev-parse --short HEAD)")
+make -C images STORAGE="${_storage}" ENV=dev VERSION="${_branch}" ${_push}
+if [ "${_branch}" = "master" ]; then
+    _branch=$(printf "%s-%s" "${_branch}" "${_sha}")
     make -C images STORAGE="${_storage}" ENV=qa VERSION="${_branch}" ${_push}
 fi
 
