380fc2b612f5061a75759b39efae1e4d76bb6ef7
Marius Orcsik <marius@federated.id>
2023-06-24 20:34:42 +0200

    Updated storage fix to cover all objects in storage
    This might backfire as it doesn't batch
    
    the collections that need to have their raw ActivityPub object stored

--- internal/cmd/fix-storage-collections.go
+++ internal/cmd/fix-storage-collections.go
@@ -17,6 +17,11 @@ var FixStorageCollectionsCmd = &cli.Command{
 }
 
 var allCollectionPaths = append(filters.FedBOXCollections, vocab.ActivityPubCollections...)
+var streamCollections = vocab.CollectionPaths{
+	filters.ActivitiesType,
+	filters.ActorsType,
+	filters.ObjectsType,
+}
 
 func newOrderedCollection(id vocab.IRI) *vocab.OrderedCollection {
 	return &vocab.OrderedCollection{
@@ -25,6 +30,16 @@ func newOrderedCollection(id vocab.IRI) *vocab.OrderedCollection {
 		Generator: ctl.Service.GetLink(),
 		Published: time.Now().UTC(),
 	}
+}
+
+func getObjectCollections(act vocab.Item) vocab.IRIs {
+	collections := make(vocab.IRIs, 0)
+	for _, col := range vocab.OfObject {
+		if colIRI := col.IRI(act); colIRI != "" {
+			collections = append(collections, colIRI)
+		}
+	}
+	return collections
 }
 
 func getActorCollections(act vocab.Item) vocab.IRIs {
@@ -42,26 +57,90 @@ func fixStorageCollectionsAct(ctl *Control) cli.ActionFunc {
 		if _, ok := ctl.Storage.(processing.CollectionStore); !ok {
 			return errors.Newf("Invalid storage type %T. Unable to handle collection operations.", ctl.Storage)
 		}
-		initialCollections := make([]vocab.IRI, 0)
-		initialCollections = append(initialCollections, getActorCollections(ctl.Service)...)
-		if ctl.Service.Streams != nil {
-			for _, stream := range ctl.Service.Streams {
-				if _, maybeCol := allCollectionPaths.Split(stream.GetLink()); !allCollectionPaths.Contains(maybeCol) {
-					ctl.Logger.Debugf("Stream doesn't seem to be a collection", stream)
-					return nil
-				}
-				initialCollections = append(initialCollections, stream.GetLink())
-			}
+		if err := tryCreateActorCollections(ctl.Service, ctl.Storage); err != nil {
+			return err
 		}
+		// NOTE(marius): this assumes that storage contains the actors, activities, objects streams collections
+		return tryCreateAllObjectsCollections(ctl.Service, ctl.Storage)
+	}
+}
 
-		for _, col := range initialCollections {
-			err := tryCreateCollection(ctl.Storage, col)
-			if err != nil {
-				continue
-			}
-		}
+func tryCreateAllObjectsCollections(actor vocab.Item, storage fedbox.FullStorage) error {
+	if actor == nil {
 		return nil
 	}
+
+	allCollections := make(vocab.IRIs, 0)
+	err := vocab.OnActor(actor, func(actor *vocab.Actor) error {
+		if actor.Streams == nil {
+			return nil
+		}
+		for _, stream := range actor.Streams {
+			_, maybeCol := allCollectionPaths.Split(stream.GetLink())
+			if !streamCollections.Contains(maybeCol) {
+				continue
+			}
+			iri := maybeCol.IRI(ctl.Service)
+			items, err := ctl.Storage.Load(iri)
+			if err != nil {
+				ctl.Logger.Debugf("Unable to load collection %s: %s", iri, err)
+				continue
+			}
+			vocab.OnCollectionIntf(items, func(col vocab.CollectionInterface) error {
+				for _, it := range col.Collection() {
+					if vocab.ActorTypes.Contains(it.GetType()) {
+						allCollections = append(allCollections, getActorCollections(it)...)
+					} else {
+						if it.IsCollection() {
+							continue
+						}
+						allCollections = append(allCollections, getObjectCollections(it)...)
+					}
+				}
+				return nil
+			})
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	for _, col := range allCollections {
+		if err := tryCreateCollection(storage, col); err != nil {
+			ctl.Logger.Warnf("Error when trying to create collection: %+s", err)
+			continue
+		}
+	}
+	return nil
+}
+
+func tryCreateActorCollections(actor vocab.Item, storage fedbox.FullStorage) error {
+	initialCollections := make([]vocab.IRI, 0)
+	initialCollections = append(initialCollections, getActorCollections(actor)...)
+	err := vocab.OnActor(actor, func(actor *vocab.Actor) error {
+		if actor.Streams == nil {
+			return nil
+		}
+		for _, stream := range actor.Streams {
+			if _, maybeCol := allCollectionPaths.Split(stream.GetLink()); !allCollectionPaths.Contains(maybeCol) {
+				ctl.Logger.Debugf("Stream doesn't seem to be a collection", stream)
+				return nil
+			}
+			initialCollections = append(initialCollections, stream.GetLink())
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	for _, col := range initialCollections {
+		err := tryCreateCollection(storage, col)
+		if err != nil {
+			ctl.Logger.Warnf("Error when trying to create collection: %+s", err)
+			continue
+		}
+	}
+	return nil
 }
 
 func tryCreateCollection(storage fedbox.FullStorage, colIRI vocab.IRI) error {
