9a601c5a81607c759b6a31685ecd872bd1b77a4c
Marius Orcsik <marius@federated.id>
2023-10-29 20:38:11 +0100

    Use coloured storage type

--- tools/all-tests.sh
+++ tools/all-tests.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
-
+RED='\033[0;31m'
+BGREEN='\033[1;32m'
+NC='\033[0m' # No Color
 run_tests() {
-  echo "Testing ${1}"
+    echo -e "Testing ${BGREEN}${1}${NC}"
     make STORAGE=${1} CGO_ENABLED=0 integration
     make FEDBOX_STORAGE=${1} CGO_ENABLED=0 integration
     make STORAGE=${1} CGO_ENABLED=1 TEST_FLAGS='-race -count=1' integration
     make FEDBOX_STORAGE=${1} CGO_ENABLED=1 TEST_FLAGS='-race -count=1' integration
+    echo ""
 }
 
 run_tests fs
@@ -14,4 +17,4 @@ run_tests sqlite
 run_tests boltdb
 run_tests badger
 
-find tests/.cache/* -type d -print -delete
+find tests/.cache/* -type d -exec rm -rf {} +
