35a204a23b01500b12c030e5580e3a50528942d3
Marius Orcsik <marius@federated.id>
2023-10-29 18:56:37 +0100

    Update routes to include profiler just in dev mode

--- routes.go
+++ routes.go
@@ -47,14 +47,14 @@ func (f FedBOX) Routes() func(chi.Router) {
 		r.Method(http.MethodGet, "/", HandleItem(f))
 		r.Method(http.MethodHead, "/", HandleItem(f))
 		// TODO(marius): we can separate here the FedBOX specific collections from the ActivityPub spec ones
-		// using some regular expressions
-		// Eg: "/{collection:(inbox|outbox|followed)}"
-		// Eg: "/{collection:(activities|objects|actors|moderators|ignored|blocked|flagged)}"
+		//   using some regular expressions
+		//   Eg: "/{collection:(inbox|outbox|followed)}"
+		//   Eg: "/{collection:(activities|objects|actors|moderators|ignored|blocked|flagged)}"
 		r.Route("/{collection}", f.CollectionRoutes(true))
 
 		r.Group(f.OAuthRoutes())
 
-		if f.conf.Env.IsDev() && f.conf.Env.IsTest() {
+		if f.conf.Env.IsDev() {
 			r.Mount("/debug", middleware.Profiler())
 		}
 
