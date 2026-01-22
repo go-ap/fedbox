2dc6af827d7975efa504808b1ba1c49469778aab
Marius Orcsik <marius@habarnam.ro>
2026-01-22 11:38:01 +0100

    Change how we build images

--- tools/build-images.sh
+++ tools/build-images.sh
@@ -2,20 +2,23 @@
 
 set -ex
 
+_action=${1:-build} # or push
+_storage=${2:-all}
+
 make -C images builder
-_storage=${1:-all}
-_push=${2:-build}
 
 _sha=$(git rev-parse --short HEAD)
-_branch=$(git branch --show-current)
-make -C images STORAGE="${_storage}" ENV=dev VERSION="${_branch}" ${_push}
+_branch=$(git branch --points-at=${_sha} | tail -n1 | tr -d '* ')
+
+_version=$(printf "%s-%s" "${_branch}" "${_sha}")
+
+make -C images STORAGE="${_storage}" ENV=dev VERSION="${_version}" "${_action}"
+
 if [ "${_branch}" = "master" ]; then
-    _branch=$(printf "%s-%s" "${_branch}" "${_sha}")
-    make -C images STORAGE="${_storage}" ENV=qa VERSION="${_branch}" ${_push}
+    make -C images STORAGE="${_storage}" ENV=qa VERSION="${_version}" "${_action}"
 fi
 
 _tag=$(git describe --long --tags || true)
 if [ -n "${_tag}" ]; then
-    make -C images STORAGE="${_storage}" ENV=prod VERSION="${_tag}" ${_push}
+  make -C images STORAGE="${_storage}" ENV=prod VERSION="${_tag}" "${_action}"
 fi
-
